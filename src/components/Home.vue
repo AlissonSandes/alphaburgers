884e6166ceb54a7efddef323ccacd0527b209798
sansontwom <alissonsandes@hotmail.com>
2020-08-16 19:26:02 -0300

    contato incompleto

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -153,8 +153,8 @@
                   <div class="row2 ma-0 pa-0">
                     <v-col class="ma-0 pa-0" no-gutters width="100vw">
                       <h1 :class="div1topstyle">O que é o Alpha Experience?</h1>
-                      <h2 class="my-2 px-12  mx-md-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h2>
-                      <h1 style="margin-top:63px">Como funciona?</h1>
+                      <h2 class="my-2 px-12  mx-md-12 bg2subt h2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h2>
+                      <h1 style="margin-top:63px" class="bg2title h1">Como funciona?</h1>
                       <h2 :class="div1bottomstyle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h2>
                       <div class="triangleleft" style="color:white;"></div>
                       <div class="triangleright" ></div>
@@ -171,11 +171,11 @@
         
         
         <v-row no-gutters>
-          <v-col class="col-12 col-sm-12 text-center pt-8 vantagens"><vantagens>Vantagens</vantagens></v-col>
+          <v-col class="col-12 col-sm-12 text-center pt-8 vantagens h1"><vantagens>Vantagens</vantagens></v-col>
           <v-col class="col-12 col-sm-10 col-lg-10 col-xl-10 offset-sm-1 offset-sm-1">
-            <div class="div3">
+            <div class="div3 pb-8">
                 <v-row no-gutters>
-                  <v-col class="col-12 col-sm-12 col-sm-6 col-md-4 text-center">
+                  <v-col class="col-12 col-sm-6 col-md-4 text-center">
                       <v-col class="text-center xs-12">
                         <i class="material-icons icon text-center">face</i>
                       </v-col>
@@ -245,23 +245,83 @@
             </div>
           </v-col>
         </v-row>
-        <v-row>
-          <v-col>
+        <v-row no-gutters>
+          <v-col class='col-xs-12 div4bg'>
             <div class="div4">
-
+                <v-col class="xs-12 px-5 pt-8 text-center">
+                  <h1 class="pt-8 pb-5 h1bg4 h1">
+                    Ainda não nos conhece?
+                  </h1>
+                  <h2 class="pb-4 h2bg4 px-8 mx-lg-8 h2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolores possimus alias quasi minima ipsum dolorem ad assumenda obcaecati facilis cupiditate temporibus!
+                  </h2>
+                </v-col>
+                <v-col class="text-center pb-8">
+                  <v-btn rounded color="white" class="txtbg4 mb-8">VER CARDÁPIO<i class="material-icons iconblack">keyboard_arrow_right</i></v-btn>
+                </v-col>
             </div>
           </v-col>
+        </v-row>
+        <v-row class="div5" no-gutters>
+            <v-col class="d-none d-md-flex" cols="12" md="4"> Aqui é onde vai o mascote, o fundo deve ser amarelo e ocupar 40% no full-screen e nada no mobile</v-col>
+            <v-col class="pb-8 my-5 ma-md-8" cols="12" md="7">
+                <div class="contato mx-2 py-4 px-2">
+                  <v-row no-gutters>
+                    <v-col class="pb-4" cols="12" md="12">
+                      <h1 class="h1">Tem alguma dúvida?</h1>
+                      <h3 class="h3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</h3>
+                    </v-col>
+                    <v-col class="d-flex pt-2 px-4 mt-0 pt-0" cols="12" md="6">
+                      <v-row>
+                        <v-col cols="12" class="py-0 my-0">
+                          <h3 class="h3 text-left">SEU NOME</h3>
+                        </v-col>
+                        <v-col cols="12" class="pt-0 my-0">
+                          <v-text-field outlined dense>
+                            nome
+                          </v-text-field>
+                        </v-col>
+                      </v-row>                 
+                    </v-col>
+                    <v-col class="d-flex pt-2 px-4 mt-0 pt-0" cols="12" md="6">
+                      <v-row>
+                        <v-col cols="12" class="py-0 my-0">
+                          <h3 class="h3 text-left">E-MAIL</h3>
+                        </v-col>
+                        <v-col cols="12" class="pt-0 mt-0">
+                          <v-text-field outlined dense>
+                            email
+                          </v-text-field>
+                        </v-col>
+                      </v-row>                 
+                    </v-col>
+                    <v-col class="d-flex pt-2 px-4 mt-0 pt-0" cols="12">
+                      <v-row no-gutters>
+                        <v-col cols="12">
+                          <h3 class="h3 text-left">MENSAGEM</h3>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-textarea outlined dense>
+
+                          </v-textarea>
+                        </v-col>
+                      </v-row>
+                    </v-col> 
+                  </v-row>  
+                  
+                </div>
+            </v-col>
         </v-row>
       </v-container>
       
       </v-content>
       <!-- RODAPE -->
-      <v-footer
-        color="indigo"
+    <!-- <v-footer
+        color="black"
         app
       >
         <span class="white--text">&copy; 2019</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -291,20 +351,20 @@
       },
       div1topstyle(){
         switch (this.$vuetify.breakpoint.name) {
-          case 'xs': return 'mt-12'
-          case 'sm': return 'mt-12 pt-5'
-          case 'md': return 'mt-12 pt-5'
-          case 'lg': return 'mt-12 pt-5'
-          case 'xl': return 'mt-12 pt-12'
+          case 'xs': return 'mt-12 bg2title h1'
+          case 'sm': return 'mt-12 pt-5 bg2title h1'
+          case 'md': return 'mt-12 pt-5 bg2title h1'
+          case 'lg': return 'mt-12 pt-5 bg2title h1'
+          case 'xl': return 'mt-12 pt-12 bg2title h1'
         }
       },
       div1bottomstyle(){
         switch (this.$vuetify.breakpoint.name) {
-          case 'xs': return ' pb-12 px-12'
-          case 'sm': return ' pt-5 mx-2 px-12'
-          case 'md': return ' pt-5 mx-12 px-12'
-          case 'lg': return ' pt-5 mx-12 px-12'
-          case 'xl': return ' pt-12 mx-12 px-12'
+          case 'xs': return ' pb-12 px-12 bg2subt h2'
+          case 'sm': return ' pt-5 mx-2 px-12 bg2subt h2'
+          case 'md': return ' pt-5 mx-12 px-12 bg2subt h2'
+          case 'lg': return ' pt-5 mx-12 px-12 bg2subt h2'
+          case 'xl': return ' pt-12 mx-12 px-12 bg2subt h2'
         }
       },
       alphaexp () {
@@ -418,15 +478,52 @@ a:hover{
 }
 .div3{
   background-color: white;
-  height: 590px;
+  /* height: 590px; */
 }
 .div4{
-  height: 372px;
-  background: linear-gradient(0.14deg, rgba(56, 56, 55, 0.65) -122.59%, rgba(192, 163, 88, 0.65) 99.6%);
+  
+  background: linear-gradient(0.14deg, rgba(56, 56, 55, 0.75) -122.59%, rgba(192, 163, 88, 0.75) 92%);
 }
 .div5{
-  height:590px;
+  width:100%;
   background: #FEAE39;
+  padding:0px;
+}
+.div4bg{
+  background-image: url('../assets/bg3.jpg');
+  background-repeat:no-repeat;
+  background-position: center;
+  background-size:cover;
+}
+.bg2title{
+  font-family: Zilla Slab;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 44px;
+  line-height: 56px;
+  /* or 127% */
+
+  text-align: center;
+
+  color: #FFFFFF;
+
+}
+.bg2subt{
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 19px;
+  line-height: 158.47%;
+  /* or 30px */
+
+  text-align: center;
+
+  color: #FFFFFF;
+
+  opacity: 0.8;
+}
+.iconblack{
+  color: #3C444C;
 }
 .triangleleft{
   /* position:absolute; */
@@ -438,6 +535,35 @@ a:hover{
   border-right: 50vw solid transparent;
   
   border-bottom: 4vw solid white;
+}
+.txtbg4{
+  font-family: Montserrat;
+  font-style: bold;
+  font-weight: 1000;
+  font-size: 14px;
+  line-height: 17px;
+  align-items: center;
+  text-transform: uppercase;
+  color: #3C444C;
+}
+.h1bg4{
+  font-family: Zilla Slab;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 44px;
+  line-height: 56px;
+  /* or 127% */
+  text-align: center;
+}
+.h2bg4{
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 19px;
+  line-height: 158.47%;
+  /* or 30px */
+  text-align: center;
+  opacity:0.8;
 }
 .triangleright{
   position:relative;
@@ -465,7 +591,6 @@ vantagens{
 vantagens.titulo{
   font-family: Zilla Slab;
   font-style: normal;
-  font-weight: normal;
   font-size: 24px;
   line-height: 28px;
   /* identical to box height, or 117% */
@@ -475,9 +600,8 @@ vantagens.titulo{
   color: #383837;
 }
 vantagens.subtitulo{
-  font-family: Montserrat;
+ font-family: Montserrat;
 font-style: normal;
-font-weight: bold;
 font-size: 14px;
 line-height: 22px;
 /* or 157% */
@@ -490,5 +614,45 @@ color: #383837;
   background-image: url('../assets/bg2.png');
   background-size: cover;
   background-position:center bottom;
+}
+.contato{
+  background-color: white;
+  border-radius:10px;
+
+}
+.contato .h1{
+  font-family: Zilla Slab;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 56px;
+  /* or 156% */
+
+  text-align: center;
+
+  color: #383837;
+  color:black;
+}
+.contato .h3{
+  font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 158.47%;
+/* or 22px */
+
+text-align: center;
+
+color: #383837;
+
+opacity: 0.8;
+}
+@media only screen and (max-width: 500px) {
+  .h1{
+    font-size:32px !important;
+  }
+  .h2{
+    font-size:16px !important;
+  }
 }
 </style>
